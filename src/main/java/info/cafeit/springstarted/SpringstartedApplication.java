8230d85f8186996c4fd98cc881932cddb29de54c
❤️Long❤️ <tranhuulongcit@gmail.com>
2020-02-11 21:24:20 +0700

    add source

---
+++ src/main/java/info/cafeit/springstarted/SpringstartedApplication.java
@@ -0,0 +1,13 @@
+package info.cafeit.springstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstartedApplication.class, args);
+	}
+
+}
