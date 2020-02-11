297ee2a64a6b2c7a66174f231048d7153bf29b22
❤️Long❤️ <tranhuulongcit@gmail.com>
2020-02-11 21:24:20 +0700

    add source

---
+++ src/test/java/info/cafeit/springstarter/SpringstartedApplicationTests.java
@@ -0,0 +1,13 @@
+package info.cafeit.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
